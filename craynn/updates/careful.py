609a23e06febb6028b21bdb958d7d96f6b421006
maxim.borisyak <mborisyak@yandex-team.ru>
2017-11-09 23:20:12 +0300

    noise updates and refactored careful updates

--- craynn/updates/careful.py
+++ craynn/updates/careful.py
@@ -4,54 +4,73 @@ import theano
 import theano.tensor as T
 
 from lasagne.updates import get_or_compute_grads, total_norm_constraint
+
+import lasagne
+
 from collections import OrderedDict
 
-def careful_rmsprop(loss_or_grads, params, learning_rate=1.0, rho=0.9, epsilon=1e-6, grad_clipping=1.0):
+__all__ = [
+  'clipped_gradients',
+  'careful',
+  'normalized_gradients',
+  'careful_rmsprop',
+  'careful_adamax',
+  'careful_adam'
+]
+
+def clipped_gradients(updates, grad_clipping=1.0, epsilon=1.0e-6):
   """
-  RMSProp with gradient clipping.
-  :param grad_clipping: maximal norm of gradient, if norm of the actual gradient exceeds this values it is rescaled.
-  :return: updates
+  Clips gradients before passing them to updates.
+
+  :param updates: any `lasagne.updates` optimizer or similar.
+  :param grad_clipping: maximal norm of the gradients.
+  :param epsilon:
+  :return:
   """
-  grads = get_or_compute_grads(loss_or_grads, params)
-  updates = OrderedDict()
-  grads = total_norm_constraint(grads, max_norm=grad_clipping, epsilon=epsilon)
+  def u(loss_or_grads, params, *args, **kwargs):
+    grads = get_or_compute_grads(loss_or_grads, params)
+    grads = total_norm_constraint(grads, max_norm=grad_clipping, epsilon=epsilon)
 
-  # Using theano constant to prevent upcasting of float32
-  one = T.constant(1)
+    return updates(grads, params, *args, **kwargs)
+  return u
 
-  for param, grad in zip(params, grads):
-    value = param.get_value(borrow=True)
-    accu = theano.shared(np.zeros(value.shape, dtype=value.dtype),
-                         broadcastable=param.broadcastable)
-    accu_new = rho * accu + (one - rho) * grad ** 2
-    updates[accu] = accu_new
-    updates[param] = param - (learning_rate * grad /
-                              T.sqrt(accu_new + epsilon))
+careful = clipped_gradients
 
-  return updates
+def normalized_gradients(updates, epsilon=1.0e-6):
+  def u(loss_or_grads, params, *args, **kwargs):
+    grads = get_or_compute_grads(loss_or_grads, params)
+    gnorm = T.sqrt(sum(T.sum(g ** 2) for g in grads) + epsilon)
+    grads = [g / gnorm for g in grads]
 
-def hard_rmsprop(loss_or_grads, params, learning_rate = 1.0e-2, epsilon=1e-6):
+    return updates(grads, params, *args, **kwargs)
+  return u
+
+def careful_rmsprop(loss_or_grads, params,  grad_clipping=1.0, learning_rate=1.0, rho=0.9, epsilon=1e-6):
   """
-  Not an actual RMSProp: just normalizes the gradient, so it norm equal to the `learning rate` parameter.
-  Don't use unless you have to.
-
-  :param loss_or_grads: loss to minimize 
-  :param params: params to optimize
-  :param learning_rate: norm of the gradient
-  :param epsilon: small number for computational stability.
-  :return: 
+  RMSProp with gradient clipping. Shortcut for `clipped_gradients(rmsprop)`.
   """
-  grads = get_or_compute_grads(loss_or_grads, params)
-  gnorm = T.sqrt(sum(T.sum(g**2) for g in grads) + epsilon)
-  grads = [ g / gnorm for g in grads ]
+  return clipped_gradients(lasagne.updates.rmsprop, grad_clipping, epsilon)(
+    loss_or_grads, params,
+    learning_rate=learning_rate, rho=rho, epsilon=epsilon
+  )
 
-  updates = OrderedDict()
+def careful_adam(loss_or_grads, params, grad_clipping=1.0, learning_rate=0.002, beta1=0.9, beta2=0.999, epsilon=1e-8):
+  """
+  Adam with gradient clipping. Shortcut for `clipped_gradients(adam)`.
+  """
+  return clipped_gradients(lasagne.updates.adam, grad_clipping, epsilon)(
+    loss_or_grads, params,
+    learning_rate=learning_rate, beta1=beta1, beta2=beta2, epsilon=epsilon
+  )
 
-  for param, grad in zip(params, grads):
-    updates[param] = param - learning_rate * grad
-
-  return updates
-
+def careful_adamax(loss_or_grads, params, grad_clipping=1.0, learning_rate=0.002, beta1=0.9, beta2=0.999, epsilon=1e-8):
+  """
+  Adamax with gradient clipping. Shortcut for `clipped_gradients(adamax)`.
+  """
+  return clipped_gradients(lasagne.updates.adamax, grad_clipping, epsilon)(
+    loss_or_grads, params,
+    learning_rate=learning_rate, beta1=beta1, beta2=beta2, epsilon=epsilon
+  )
 
 def cruel_rmsprop(loss_or_grads, params, learning_rate=1.0, rho=0.9, epsilon=1e-6,
                   grad_clipping=1.0e-2, param_clipping=1.0e-2):
