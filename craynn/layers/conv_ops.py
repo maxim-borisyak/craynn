7f9b25b484bc1399aad7ed26739ea1fe8a2b7b3c
Maxim Borisyak <mborisyak@yandex-team.ru>
2018-02-05 19:30:07 +0300

    1D ops + py2 support

--- craynn/layers/conv_ops.py
+++ craynn/layers/conv_ops.py
@@ -6,13 +6,20 @@ __all__ = [
   'upscale',
   'max_pool', 'mean_pool', 'floating_maxpool', 'floating_meanpool',
   'global_pool',
-  'upconv', 'downconv'
+  'upconv', 'downconv',
+  'conv_1D', 'conv1x1_1D', 'max_pool_1D', 'upscale_1D'
 ]
+
+### common ops
 
 get_conv_nonlinearity = lambda f=None: nonlinearities.LeakyRectify(0.05) if f is None else f
 
 get_companion_nonlinearity = lambda num_units=None, f=None: \
   nonlinearities.sigmoid if num_units is None or num_units == 1 else nonlinearities.softmax
+
+global_pool = lambda f=None: lambda incoming: layers.GlobalPoolLayer(incoming, pool_function=f)
+
+### 2D ops
 
 conv = lambda num_filters, f=None, filter_size=(3, 3): lambda incoming: layers.Conv2DLayer(
   incoming,
@@ -74,10 +81,35 @@ mean_pool = lambda pool_size=(2, 2): lambda incoming: layers.Pool2DLayer(incomin
 
 floating_meanpool = lambda pool_size=(2, 2): lambda incoming: layers.Pool2DLayer(
   incoming,
-  pool_size=(pool_size[0] // 2 * 3, pool_size[0] // 2 * 3),
+  pool_size=(pool_size[0] // 2 * 3, pool_size[1] // 2 * 3),
   stride=pool_size,
   pad=(pool_size[0] // 2, pool_size[1] // 2),
   mode='average_inc_pad'
 )
 
-global_pool = lambda f=None: lambda incoming: layers.GlobalPoolLayer(incoming, pool_function=f)
+### 1D ops
+
+conv_1D = lambda num_filters, f=None, filter_size=(3,): lambda incoming: layers.Conv1DLayer(
+  incoming,
+  num_filters=num_filters, filter_size=filter_size,
+  nonlinearity=get_conv_nonlinearity(f),
+  pad='valid'
+)
+
+conv1x1_1D = lambda num_filters, f=None: lambda incoming: layers.Conv1DLayer(
+  incoming,
+  num_filters=num_filters, filter_size=(1, ),
+  nonlinearity=get_conv_nonlinearity(f),
+  pad='valid'
+)
+
+max_pool_1D = lambda pool_size=(2,): lambda incoming: layers.MaxPool1DLayer(incoming, pool_size=pool_size)
+
+floating_maxpool_1D = lambda pool_size=(2,): lambda incoming: layers.MaxPool1DLayer(
+  incoming,
+  pool_size=(pool_size[0] // 2 * 3, ),
+  stride=pool_size,
+  pad=(pool_size[0] // 2,)
+)
+
+upscale_1D = lambda scale_factor=(2, ): lambda incoming: layers.Upscale1DLayer(incoming, scale_factor=scale_factor)
